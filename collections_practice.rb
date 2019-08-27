ae428906c80e7d0b61ec36ad4dcdc5c440f95ccb
Baktiar Miah <baktiar.m@gmail.com>
2019-08-27 14:50:50 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,63 @@
+def sort_array_desc(array)
+  return array.sort.reverse
+end
 
+def sort_array_char_count(arr)
+
+end 
+
+def swap_elements(arr)
+
+end
+
+def reverse_array(arr)
+  arr.reverse
+end 
+
+def kesha_maker(arr)
+ newArr=[] 
+  arr.each do |word|
+ newArr.push(word[2] <<  '$')
+ end
+ newArr
+end 
+
+def find_a(arr)
+  newArr=[]
+  arr.each do |words|
+    if words.start_with? |'a'|
+      newArr.push(words)
+    end 
+  end 
+  newArr
+end
+    
+    
+def sum_array(array)
+sum = 0
+array.each { |a| sum+=a }
+sum
+end
+
+def add_s(arr)
+  arr.collect do |words|
+    words << 's'
+  end
+  arr[1] = 'feet'
+  arr
+end
+  
+  
+  
+  
+  
+  
+  
+  
+  
+    
+    
+    
+    
+    
+    
